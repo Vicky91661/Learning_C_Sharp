3aaf1a81cb32d567a41f44bb188a925b4ed066e3
Vicky91661 <kumar.vickysah72@gmail.com>
2024-07-18 12:54:46 +0530

    Learning the sytex of C sharp

---
+++ Program1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello Vicky!");
+string firstFriend = "Raja";
+string secondFriend = "Supon";
+
+Console.WriteLine($"My friends are {firstFriend} and {firstFriend.Length}");
+Console.WriteLine($"My friends are {secondFriend} and {secondFriend.Length}");//$ => String Interpolation 
+
+
+string Firstname  = "   Vicky     s";
+Console.WriteLine($"Before triming my First name is {Firstname}");
+string LastName = "      sah     ";
+Console.WriteLine($"my First name is {Firstname.TrimStart()}");
+
+string friends = $"My friends are {firstFriend} and {secondFriend}";
+
+Console.WriteLine(friends);
+Console.WriteLine(friends.Replace("Supon","Nidhi"));
+Console.WriteLine(friends);
+Console.WriteLine(friends.Contains("Supon"));
+Console.WriteLine(friends.ToUpper());
+
+
+int a = 18;
+int b=6;
+int c = a+b ;
+
+Console.WriteLine(c);
